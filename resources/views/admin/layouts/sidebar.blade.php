65a68106b507ffee79816b1fecb96385d7e1787a
irobiul56 <irobiul56@gmail.com>
2022-08-05 10:33:11 +0600

    Dasboard Tamplete setup

---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,57 @@
+			<!-- Sidebar -->
+            <div class="sidebar" id="sidebar">
+                <div class="sidebar-inner slimscroll">
+					<div id="sidebar-menu" class="sidebar-menu">
+						<ul>
+							<li class="menu-title"> 
+								<span>Main</span>
+							</li>
+							<li> 
+								<a href="#"><i class="fe fe-home"></i> <span>Dashboard</span></a>
+							</li>
+                            <li> 
+								<a href="#"><i class="fe fe-desktop"></i> <span>Slider</span></a>
+							</li>
+                            <li> 
+								<a href="#"><i class="fe fe-commenting"></i> <span>Testimonials</span></a>
+							</li>
+                            <li> 
+								<a href="#"><i class="fe fe-user"></i> <span>Our Clients</span></a>
+							</li>
+                            <li> 
+                                <li class="submenu">
+                                    <a href="#"><i class="fe fe-beginner"></i> <span> Protfolio</span> <span class="menu-arrow"></span></a>
+                                    <ul style="display: none;">
+                                        <li><a href="#">Protfolio</a></li>
+                                        <li><a href="#">Tag</a></li>
+                                        <li><a href="#">Categories</a></li>
+                                    </ul>
+                                </li>
+							</li>
+                            <li> 
+								<a href="#"><i class="fe fe-users"></i> <span>Our Team</span></a>
+							</li>
+							<li class="submenu">
+								<a href="#"><i class="fe fe-document"></i> <span> Posts</span> <span class="menu-arrow"></span></a>
+								<ul style="display: none;">
+									<li><a href="#">All Posts</a></li>
+									<li><a href="#">Categories</a></li>
+									<li><a href="#">Tag</a></li>
+								</ul>
+							</li>
+                            <li class="menu-title"> 
+								<span>Admin Option</span>
+							</li>
+                            <li class="submenu">
+								<a href="#"><i class="fe fe-check-verified"></i> <span> Admin User</span> <span class="menu-arrow"></span></a>
+								<ul style="display: none;">
+									<li><a href="#">Users</a></li>
+									<li><a href="#">Role</a></li>
+									<li><a href="#">Permission</a></li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+                </div>
+            </div>
+			<!-- /Sidebar -->

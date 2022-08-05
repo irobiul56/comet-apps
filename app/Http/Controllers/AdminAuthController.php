8b3ca4c794d003c913421429ceeb2b10b248d4df
irobiul56 <irobiul56@gmail.com>
2022-08-05 16:54:02 +0600

    Multiple login username done

--- app/Http/Controllers/AdminAuthController.php
+++ app/Http/Controllers/AdminAuthController.php
@@ -22,7 +22,7 @@ class AdminAuthController extends Controller
         'password'  => 'required'
        ]);
 
-       if (Auth::guard('admin') -> attempt(['email' => $request -> auth, 'password' => $request -> password])) {
+       if (Auth::guard('admin') -> attempt(['email' => $request -> auth, 'password' => $request -> password]) || Auth::guard('admin') -> attempt(['cell' => $request -> auth, 'password' => $request -> password]) || Auth::guard('admin') -> attempt(['username' => $request -> auth, 'password' => $request -> password])) {
         
         return redirect() -> route('admin.Dasboard.page');
 
